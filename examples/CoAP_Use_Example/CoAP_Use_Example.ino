e4468c7832697e9dba117300a699761027d8efd9
Johan Zander <johanzander@gmail.com>
2015-05-24 23:49:44 +0200

    Updated example to use WiFi

--- examples/CoAP_Use_Example/CoAP_Use_Example.ino
+++ examples/CoAP_Use_Example/CoAP_Use_Example.ino
@@ -1,66 +1,91 @@
 /**
- * Arduino Coap Example Application.
- *
- * This Example creates a CoAP server with 2 resources.
- * resGET : A resource that contains an integer and the GET method is only available.
- *          A GET request returns the value of the value_get variable.
- * resGET-POST : A resource that contains an integer and the GET-POST methods are available.
- *               A GET request returns the value of the value_post variable.
- *               A POST request sets the value of value_post to the sent integer.
- * Both resources are of TEXT_PLAIN content type.
- */
+* Arduino Coap Example Application using the standard WiFi library.
+*
+* This Example creates a CoAP server with 2 resources.
+* 'button' : A resource that contains an integer and the GET method is only available.
+*            A GET request returns the value of the value_get variable.
+The resource is OBSERVEable.
+* 'led'    : A resource that contains an integer and the GET-POST methods are available.
+*            A GET request returns the value of the value_post variable.
+*            A POST request sets the value of value_post to the sent integer.
+* All resources are of TEXT_PLAIN content type.
+*/
 
-
-//Include XBEE Libraries
-#include <XBee.h>
-#include <XbeeRadio.h>
-
-//Include CoAP Libraries
+#include <SPI.h>
+#include <WiFi.h>
 #include <coap.h>
 #include "myGETSensor.h"
 #include "myPOSTSensor.h"
+#include "WiFiTransport.h"
 
-//Create the XbeeRadio object we'll be using
-XBeeRadio xbee = XBeeRadio();
-//Create a reusable response object for responses we expect to handle
-XBeeRadioResponse response = XBeeRadioResponse();
-//Create a reusable rx16 response object to get the address
-Rx16Response rx = Rx16Response();
+// WiFi stuff
+char ssid[] = "*********";    //  your network SSID (name)
+char pass[] = "*********";    // your network password (use for WPA, or use as key for WEP)
+int status = WL_IDLE_STATUS;
 
-//CoAP object
+#define PORT 5683
+
 Coap coap;
+WiFiClient client(255);
+WiFiUDP udp;
+WiFiTransport transport(&udp);
 
-myGETSensor aSensor = myGETSensor("resGET1" , A0);
-myPOSTSensor bSensor = myPOSTSensor("resGET-POST" , 3);
-
+//myGETSensor aSensor = myGETSensor("light" , A0, false);
+myPOSTSensor bSensor = myPOSTSensor("led" , 3);
+myGETSensor cSensor = myGETSensor("button" , 8, true);
 
 //Runs only once
 void setup()
 {
+int i;
+Serial.begin(9600);
+while (!Serial)
+{
+; // wait for serial port to connect. Needed for Leonardo only
+}
 
-  pinMode(3, OUTPUT);     
-  
-  // comment out for debuging
-  xbee.initialize_xbee_module();
-  //start our XbeeRadio object and set our baudrate to 38400.
-  xbee.begin( 38400 );
-  //Initialize our XBee module with the correct values (using the default channel, channel 12)h
-  xbee.init(12);
+// attempt to connect to Wifi network:
+while (status != WL_CONNECTED) {
+Serial.print("Attempting to connect to SSID: ");
+Serial.println(ssid);
+// Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+status = WiFi.begin(ssid, pass);
 
-  // init coap service 
-  coap.init( &xbee, &response, &rx );
+// wait 5 seconds for connection:
+delay(5000);
+}
+Serial.println("Connected to wifi");
+printWifiStatus();
+udp.begin(PORT);
 
-  //add the resourse resGET
-  coap.add_resource(&aSensor);
-  coap.add_resource(&bSensor);
-  //add the resources resGET-POST
-  //coap.add_resource("resGET-POST"  , GET | POST, &handlerGET_POST, true, 20, TEXT_PLAIN);
+// init coap service
+coap.init( &transport );
 
-
+//add the resourse resGET
+//  coap.add_resource(&aSensor);
+coap.add_resource(&bSensor);
+coap.add_resource(&cSensor);
 }
 
 void loop()
 {
-  //run the handler on each loop to respond to incoming requests
-  coap.handler();
+//run the handler on each loop to respond to incoming requests
+coap.handler();
+}
+
+void printWifiStatus() {
+// print the SSID of the network you're attached to:
+Serial.print("SSID: ");
+Serial.println(WiFi.SSID());
+
+// print your WiFi shield's IP address:
+IPAddress ip = WiFi.localIP();
+Serial.print("IP Address: ");
+Serial.println(ip);
+
+// print the received signal strength:
+long rssi = WiFi.RSSI();
+Serial.print("signal strength (RSSI):");
+Serial.print(rssi);
+Serial.println(" dBm");
 }
